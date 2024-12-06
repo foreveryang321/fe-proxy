3780396712dbfc1a847f01c241f4804ab66dce05
foreveryang321 <foreveryang321@gmail.com>
2024-12-06 16:47:05 +0800

    分离 v2 和 v3 配置

--- v2/pub.sh
+++ v2/pub.sh
@@ -1,3 +1,3 @@
-rm fe-proxy-v1.0.0.zip
-zip -r fe-proxy-v1.0.0.zip dist
+rm fe-proxy-v1.0.0.manifest-v2.zip
+zip -r fe-proxy-v1.0.0.manifest-v2.zip dist
 #open https://chrome.google.com/webstore/devconsole/xxx/xxx/edit/package?hl=zh_CN
