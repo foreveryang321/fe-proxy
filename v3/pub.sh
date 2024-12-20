c089a9e38f737d8fd8e62b0318921ffd402a8452
foreveryang321 <foreveryang321@gmail.com>
2024-12-20 10:52:09 +0800

    feat：上传 v3 版本部分代码

--- v3/pub.sh
+++ v3/pub.sh
@@ -1,3 +1,3 @@
-rm fe-proxy-2.0.0.manifest-v2.zip
-zip -r fe-proxy-2.0.0.manifest-v2.zip dist
+rm fe-proxy-2.0.0.manifest-v3.zip
+zip -r fe-proxy-2.0.0.manifest-v3.zip dist
 #open https://chrome.google.com/webstore/devconsole/xxx/xxx/edit/package?hl=zh_CN
